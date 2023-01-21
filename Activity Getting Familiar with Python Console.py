912695d8a5eabbb4c25f5fd9bd37ce5fc7185066
yasar12123 <123254904+yasar12123@users.noreply.github.com>
2023-01-21 15:26:08 +0000

    Add files via upload

---
+++ Activity Getting Familiar with Python Console.py
@@ -0,0 +1,27 @@
+#0
+    #item_location = input("is the item located outside of the UK?")
+    #item_type = input("is the item a book or document?")
+    #purchase_price = float(input("what is the purchase price?"))
+        #if item_location == "yes" and item_type == "no": print("VAT required, The total cost of the item is: " + str(purchase_price*1.2))
+        #else: print("No VAT required, The total cost of the item is: " + str(purchase_price))
+
+
+
+#1. convert hours into minutes#
+    #hours = float(input("what is the number of hours you want to convert to minutes"))
+    #print("There are " + str(hours*60) + " minutes in " + str(hours) + " hours")
+
+
+#2. cost of expenses
+loop = int(input("Number of Expenses: "))
+expenses_name = []
+expenses_list = []
+count = -1
+for expen_name_list in range(loop):
+    count+=1
+    expenses_name.append(input("Name of Expenditure: "))
+    expenses_list.append(input("Cost of " + str(expenses_name[count]) + ": " ))
+
+print(sum([float(x) for x in expenses_list]))
+
+
---
+++ flowControl.py
@@ -0,0 +1,20 @@
+#even odd number
+#number = float(input("Input a number: "))
+#result = number % 2
+
+#if result == 0:
+    #print(str(number) + "is a even number")
+#else: print(str(number) + "is a odd number")
+
+
+
+#number 1 multiple of number 2
+number1 = float(input("Input number 1: "))
+number2 = float(input("Input number 2: "))
+
+result = (number2/number1) % 1
+
+if result == 0:
+    print(str(number1) + " is a multiple of " + str(number2) )
+else:
+    print(str(number1) + " is not a multiple of " + str(number2) )
