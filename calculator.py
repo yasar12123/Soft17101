6aad0fe47afa177795bd2a0f62848d1615e3397b
yasar12123 <123254904+yasar12123@users.noreply.github.com>
2023-01-25 03:30:07 +0000

    v5

--- calculator.py
+++ calculator.py
@@ -2,48 +2,80 @@
 #this function adds the inputs
 def addition(x, y):
     calc = float(x) + float(y)
-    print(calc)
+    print(x, " + ", y, " = ", calc)
+
 
 #this function adds subtracts the inputs
 def subtraction(x, y):
     calc = float(x) - float(y)
-    print(calc)
+    print(x, " - ", y, " = ", calc)
+
 
 #this function multiply's the inputs
 def multiplication(x, y):
     calc = float(x) * float(y)
-    print(calc)
+    print(x, " * ", y, " = ", calc)
+
 
 #this function divides the inputs
 def division(x, y):
     calc = float(x) / float(y)
-    print(calc)
+    print(x, " / ", y, " = ", calc)
 
 
-functionSelect = input("Select an operation: " +
-                        '\n' "(1) Add" +
-                         '\n' "(2) Subtract" +
-                         '\n' "(3) Multiply" +
-                         '\n' "(4) Divide)")
+#this function takes x to the power of y
+def exponent(x, y):
+    calc = float(x) ** float(y)
+    print(x, " ^ ", y, " = ", calc)
 
-if functionSelect not in ["1", "2", "3", "4"]:
-    print("Error!, that is not a function")
 
-else:
-    numberX = input("Enter a first number: ")
-    if isinstance(float(numberX), float) == False:
-        print("That is not a number: ")
+contCalc = False
+
+
+while contCalc is False:
+    functionSelect = input('\n' "Select an operation: " +
+                           '\n' "(1) Add" +
+                           '\n' "(2) Subtract" +
+                           '\n' "(3) Multiply" +
+                           '\n' "(4) Divide" +
+                           '\n' "(5) Exponent" '\n')
+
+    if functionSelect not in ["1", "2", "3", "4", "5"]:
+        print("Error!, that is not an operation in the list")
 
     else:
-        numberY = input("Enter a second number: ")
-        if isinstance(float(numberY), float) == False:
-            print("That is not a number: ")
-        else:
-            if functionSelect == "1":
-                addition(numberX, numberY)
-            elif functionSelect == "2":
-                subtraction(numberX, numberY)
-            elif functionSelect == "3":
-                multiplication(numberX, numberY)
-            elif functionSelect == "4":
-                division(numberX, numberY)
+        #error handling number input 1
+        while True:
+            numberX = input("Enter first number: ")
+            try:
+                check = int(numberX) or float(numberX)
+                break;
+            except ValueError:
+                print("That is not a valid number, try again")
+        # error handling number input 2
+        while True:
+            numberY = input("Enter second number: ")
+            try:
+                check = int(numberY) or float(numberY)
+                break;
+            except ValueError:
+                print("That is not a valid number, try again")
+
+    if functionSelect == "1":
+        addition(numberX, numberY)
+    elif functionSelect == "2":
+        subtraction(numberX, numberY)
+    elif functionSelect == "3":
+        multiplication(numberX, numberY)
+    elif functionSelect == "4":
+        division(numberX, numberY)
+    elif functionSelect == "5":
+        exponent(numberX, numberY)
+
+    nextOperation = input('\n' "Would you like to perform another operation : "
+                          '\n' "Yes or No" '\n')
+    if nextOperation.upper() == "YES":
+        contCalc = False
+    else:
+        contCalc = True
+
--- test.py
+++ test.py
@@ -1,32 +1,5 @@
-
-#this function adds the inputs
-def addition(x, y):
-    calc = int(x) + int(y)
-    print(calc)
-
-#this function adds subtracts the inputs
-def subtraction(x, y):
-    calc = int(x) - int(y)
-    print(calc)
-
-#this function multiply's the inputs
-def multiplication(x, y):
-    calc = int(x) * int(y)
-    print(calc)
-
-#this function divides the inputs
-def division(x, y):
-    calc = int(x) / int(y)
-    print(calc)
+calc = "2**4"
 
 
-functionSelectInput = False
 
-while functionSelectInput is False:
-    functionSelect = input("Select an operation: " +
-                            '\n' "(1) Add" +
-                            '\n' "(2) Subtract" +
-                            '\n' "(3) Multiply" +
-                            '\n' "(4) Divide)")
-    if functionSelect not in ["1","2", "3", "4"]:
-        print("Error!, that is not a function")
+print(eval(calc))
