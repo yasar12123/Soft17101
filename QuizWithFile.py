24f37687ad742dd6de60c4c3a38f1c54019ab4a5
yasar12123 <123254904+yasar12123@users.noreply.github.com>
2023-01-28 02:36:14 +0000

    v7

--- QuizWithFile.py
+++ QuizWithFile.py
@@ -1,57 +1,73 @@
 import os
+from datetime import datetime
+
+#This section lists all paths and files
 path = r'C:\Users\yh28\PycharmProjects\Python Project'
-fileQQ = 'quiz questions.txt'
-quizQuestions = os.path.join(path, fileQQ)
-my_file = open(quizQuestions, encoding='utf-8')
+fileQuiz = 'quiz.txt'
+fileScores = 'quizScores.txt'
+quizFullPath = os.path.join(path, fileQuiz)
+scoresFullPath = os.path.join(path, fileScores)
+quiz = open(quizFullPath, "w")
+quizScores = open(scoresFullPath, "a")
 
+#all questions in the quiz
+numberOfQuestions = 3
+questionCounter = 1
+question1 = ('\n' "What is the capital of United Kingdom?" +
+             '\n' "A.London" +
+             '\n' "B.Paris" +
+             '\n' "C.Washington" +
+             '\n' "D.Madrid")
+
+question2 = ('\n' "What is the most widely used currency in the European continent?" +
+             '\n' "A.Pound" +
+             '\n' "B.Euro" +
+             '\n' "C.Dollar" +
+             '\n' "D.Rand")
+
+question3 = ('\n' "Who is the main actor in the Mission Impossible movies?" +
+             '\n' "A.Daniel Craig" +
+             '\n' "B.Tom Cruise" +
+             '\n' "C.Mr Bean" +
+             '\n' "D.James Bond")
+
+#answers for the questions above in order
+questionAnswerList = ["A", "B", "B"]
+questionAnswerListCounter = 0
+
+#
+attemptCounter = 0
 totalScore = 0
-correctAnswer = "\nThat is the correct answer"
-incorrectAnswer = "\nThat is the wrong answer"
-attemptCounter = 0
 
-while (attemptCounter < 3):
-    q1 = input(readlines()[start:end])
-    q1_upper = q1.upper()
 
-    if q1_upper == "A":
-        print(correctAnswer)
-        totalScore += 1
-        break
-    else:
-        print(incorrectAnswer)
-        attemptCounter += 1
-        print("Attemt Number: ", attemptCounter)
+#start of the program
+userName = input("What is your name? ")
 
-attemptCounter = 0
+for quizLoop in range(numberOfQuestions):
+    while attemptCounter < 3:
+        question = input(eval(("question%s" % questionCounter)))
+        q_upper = question.upper()
 
-while (attemptCounter < 3):
-    q2 = input("\nWhat is the most widely used currency in the European continent? \n A.Pound \n B.Euro \n C.Dollar \n D.Rand \n \n What is your answer? " )
-    q2_upper = q2.upper()
+        if q_upper == questionAnswerList[questionAnswerListCounter]:
+            print("\nThat is the correct answer")
+            totalScore += 1
+            break
+        else:
+            print("\nThat is the wrong answer")
+            attemptCounter += 1
+            print("Attempt Number: ", attemptCounter)
 
-    if q2_upper == "B":
-        print(correctAnswer)
-        totalScore += 1
-        break
-
-    else:
-        print(incorrectAnswer)
-        attemptCounter += 1
-        print("Attemt Number: ", attemptCounter)
-
-attemptCounter = 0
-
-while (attemptCounter < 3):
-    q3 = input("\nWhat Colour is the grass? \n A.Blue \n B.Yellow \n C.Green \n D.Red \n \n What is your answer? " )
-    q3_upper = q3.upper()
-
-    if q3_upper == "C":
-        print(correctAnswer)
-        totalScore += 1
-        break
-
-    else:
-        print(incorrectAnswer)
-        attemptCounter += 1
-        print("Attemt Number: ", attemptCounter)
+    quiz.write('\n' + eval(("question%s" % questionCounter)) +
+               '\n' "The correct answer is: %s" % questionAnswerList[questionAnswerListCounter])
+    questionCounter += 1
+    questionAnswerListCounter += 1
+    attemptCounter = 0
 
 print("\nYour total score is ", totalScore)
+#end of program
+
+
+#write scores to file and close any open files
+quiz.close()
+quizScores.write('\n' 'Player: {} ; Score: {} ; DateTime: {}' .format(userName, totalScore, datetime.now()))
+quizScores.close()
--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-import os
-import re
-path = r'C:\Users\yh28\PycharmProjects\Python Project'
-fileQQ = 'quiz questions.txt'
-fullPath = os.path.join(path, fileQQ)
-quizQuestions = open(fullPath, encoding='utf-8')
+from datetime import datetime
+curr_dt = datetime.now()
 
-try:
-    for line in quizQuestions:
-        if "Question" in line:
-            print(line)
-finally:
-    quizQuestions.close()
+print("Current datetime: ", datetime.now())
