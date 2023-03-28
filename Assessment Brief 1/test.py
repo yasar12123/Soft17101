a045261deefaaf12d375374f3eed55896ae93d48
yasar12123 <123254904+yasar12123@users.noreply.github.com>
2023-03-28 11:37:54 +0100

    Delete test.py

--- Assessment Brief 1/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#This section lists paths and files
-path = r'C:\Users\yh28\PycharmProjects\Python Project'
-fileEPL = 'epl.txt'
-EPLFullPath = os.path.join(path, fileEPL)
-
-for line in open(EPLFullPath):
-    print(line)
